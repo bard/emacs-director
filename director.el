49af4056b7ad1ce868245b5f8b211970bd6871cb
Massimiliano Mirra <bard@hyperstruct.net>
2021-02-22 10:21:38 +0000

    docs: document entry point

--- director.el
+++ director.el
@@ -47,30 +47,46 @@
 (defun director-run (&rest config)
   "Simulate a user session as defined by CONFIG.
 
-CONFIG is a property list containing the following properties:
+CONFIG is a property list containing the following properties and
+their values:
 
-- `:version' (number; required): currently must be `1'
-- `:steps' (list; required): a list of steps (see below)
-- `:before-start':
-- `:after-end':
-- `:after-step':
-- `:on-failure':
-- `:on-error':
-- `:on-failure':
-- `:log-target':
-- `:typing-style':
-- `:delay-between-steps':
+- `:version': required number indicating the config format
+  version; must be `1'
+- `:steps': required list of steps (see below for the step
+  format)
+- `:before-start' : optional function to run before the first
+  step
+- `:after-end' optional function to run after the last step 
+- `:after-step' optional function to run after every step
+- `:on-failure': optional function to run when an `:assert' step
+  fails
+- `:on-error': optional function to run when a step triggers an
+  error
+- `:log-target': optional cons cell of the format `(file
+  . \"filename\")' specifying a file to save the log to
+- `:typing-style': optional symbol changing the way that `:type'
+  steps type characters; set to `human' to simulate a human
+  typing
+- `:delay-between-steps': optional number specifying how many
+  seconds to wait after a step; defaults to `1'; set lower for
+  automated tests
 
 A step can be one of:
 
-- `:type':
-- `:call':
-- `:log':
-- `:wait':
-- `:suspend':
-- `:assert':
-
-"
+- `:type': simulate typing text; can be a string or a vector of
+  key events; if a string, it will be converted to key events
+  using `listify-key-sequence' and can contain special
+  characters, e.g. `(:type \"\\M-xsetenv\\r\")'
+- `:call': shortcut to invoke an interactive command, e.g. `(:call setenv)'
+- `:log': Lisp form; it will be evaluated and its result will be
+  written to log; e.g. `(:log (buffer-file-name (current-buffer)))'
+- `:wait': number; seconds to wait before next step; overrides
+  config-wide `:delay-between-steps'
+- `:assert': Lisp form; if it evaluates to `nil', execution is
+  interrupted and function configured through `:on-failure' is
+  called 
+- `:suspend': suspend execution; useful for debugging; resume
+  using the `director-continue' command"
   (director--read-config config)
   (setq director--start-time (float-time))
   (director--before-start)
